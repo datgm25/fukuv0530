17217e249530613cf1f1d82bf041fca7fd4b728b
YuTanaka <tanakaedu@users.noreply.github.com>
2025-05-30 14:11:59 +0900

    完成

--- fukuv0530/Form1.cs
+++ fukuv0530/Form1.cs
@@ -2,9 +2,27 @@ namespace fukuv0530
 {
     public partial class Form1 : Form
     {
+        int vx = 5;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += vx;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            vx = -vx;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            label1.Text = "Tanaka";
         }
     }
 }
